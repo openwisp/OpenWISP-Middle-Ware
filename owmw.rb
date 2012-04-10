fcc8caf9f325a195baecb649dacc45d9d5327da7
Davide Guerri <davide.guerri@gmail.com>
2012-04-10 13:16:30 +0000

    adds caching to associated_users.
    It's typical for any recent OS/UA to activate multiple HTTP connections when a network connections becomes available: each of this connections generates a middleware query that, in turn, generates a query to the openVPN management service and a query to OWUMS.
    This caching strategy reduces the overall load of the aforementioned process and mitigates DoS attacks.
    
    Note: another (although more complex) way to achieve this could be caching (page not http caching).

--- owmw.rb
+++ owmw.rb
@@ -56,16 +56,22 @@ get '/access_points/:name/associated_users.xml' do
 end
 
 get '/associated_users/:mac_address.xml' do
-  vpn_server = OpenVpn.new(settings.vpns_to_scan)
-  cn = vpn_server.find_client_cname_by_associated_mac_address(params[:mac_address])
+  if !defined?(@@owmw_cache) || @@owmw_cache[params[:mac_address]].nil?
+    vpn_server = OpenVpn.new(settings.vpns_to_scan)
+    cn = vpn_server.find_client_cname_by_associated_mac_address(params[:mac_address])
 
-  @associated_user = AssociatedUser.new.load(
-    :access_point => AccessPoint.find(cn)
-  )
+    @associated_user = AssociatedUser.new.load(
+        :access_point => AccessPoint.find(cn)
+    )
 
-  if @associated_user
-    @associated_user.to_xml
+    if @associated_user
+      # Initialize cache with 60 seconds of duration with memoization
+      @@owmw_cache = Cache.new 60 unless defined?(@@owmw_cache)
+      @@owmw_cache[params[:mac_address]] = @associated_user.to_xml
+    else
+      404
+    end
   else
-    404
+-    @@owmw_cache[params[:mac_address]]
   end
 end
